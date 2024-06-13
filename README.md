dc0515205072365503656a692df15baa16348c70
Eddie Nguyen <edward_nguyen@brown.edu>
2024-06-13 06:00:20 +0000

    rm: nix build/run

--- README.md
+++ README.md
@@ -3,17 +3,16 @@ Minimal template for Rust projects to be used with Nix developement environments
 
 ### Requirements
 - Flakes-enabled installation of [Nix](https://nixos.org/download/)
-- [cargo-generate](https://crates.io/crates/cargo-generate)
 
 ### Installation
-```shell
+I like to use [cargo-generate](https://crates.io/crates/cargo-generate)
+```bash
 cargo generate eddien24/rust-template
 ```
 
 ### Usage
 - `nix develop`: start developement shell.
   - First launch: need to `git add .` so it creates `flake.lock`
-- `nix build`: build project
-- `nix run`: run project
+- Use `cargo` as you would for building, testing, etc
 
 Modified from [MordragT/nix-templates](https://github.com/MordragT/nix-templates/tree/master/rust-stable).
--- flake.nix
+++ flake.nix
@@ -18,21 +18,7 @@
         toolchain = pkgs.rustPlatform;
       in rec
       {
-        # Executed by `nix build`
-        packages.default = toolchain.buildRustPackage {
-          pname = "template";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-
-          # For other makeRustPlatform features see:
-          # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
-        };
-
-        # Executed by `nix run`
-        apps.default = utils.lib.mkApp {drv = packages.default;};
-
-        # Used by `nix develop`
+       # Used by `nix develop`
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             (with toolchain; [
